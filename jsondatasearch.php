02fe2b00e0e7a93d3e3328ae0ae00ea38c2fe7f7
sjodi442 <sjodi44@localhost.localdomain>
2017-03-21 19:37:59 +0700

    seccond commit

--- jsondatasearch.php
+++ jsondatasearch.php
@@ -1,11 +1,23 @@
 <?php
 $link=mysqli_connect("localhost","root","","db_kelas");
-$get = $_GET['teg'];
-$query = "select * from tb_siswa where nama_siswa = '$get'";
-$sql = mysqli_query($link, $query);
-while($row=mysqli_fetch_assoc($sql)){
-$emparray = array();
-    $emparray[] = $row;
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  $get = $_GET['teg'];
+  $query = "select * from tb_siswa where nama_siswa = '$get'";
+  $sql = mysqli_query($link, $query);
+  while($row=mysqli_fetch_assoc($sql)){
+  $emparray = array();
+      $emparray[] = $row;
+  }
+  echo json_encode($emparray);
 }
-echo json_encode($emparray);
+elseif($_SERVER['REQUEST_METHOD'] === 'POST'){
+  $get = $_POST['teg'];
+  $query = "select * from tb_siswa where nama_siswa = '$get'";
+  $sql = mysqli_query($link, $query);
+  while($row=mysqli_fetch_assoc($sql)){
+  $emparray = array();
+      $emparray[] = $row;
+  }
+  echo json_encode($emparray);
+}
  ?>
